b4d3a95251a1910fec7c4dc321046b4143fc071a
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-05-15 17:49:18 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get('/', (req, res) => {
     Restaurant.find()
         .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
         .then(restaurant => res.render('index', { restaurant })) // 將資料傳給 index 樣板
+        .sort({ _id: 'asc' })
         .catch(error => console.error(error)) // 錯誤處理
 })
 
