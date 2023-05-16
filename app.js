20c59ac0d0ef963b22d0411c63c84756b2c84a77
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-05-16 11:21:17 +0800

    feat: add method-override in app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars');
 const Restaurant = require('./models/Restaurant')
 const bodyParser = require('body-parser')
+const routes = require('./routes') // 引用路由器
+const methodOverride = require('method-override') 
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
@@ -17,6 +19,8 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(methodOverride('_method'))
+app.use(routes) // 將 request 導入路由器
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -34,15 +38,6 @@ const port = 3003
 
 app.use(express.static('public'))
 
-// index
-app.get('/', (req, res) => {
-    Restaurant.find()
-        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-        .then(restaurant => res.render('index', { restaurant })) // 將資料傳給 index 樣板
-        .sort({ _id: 'asc' })
-        .catch(error => console.error(error)) // 錯誤處理
-})
-
 // search
 app.get('/search', (req, res) => {
     const keyword = req.query.keyword
@@ -54,80 +49,6 @@ app.get('/search', (req, res) => {
                 .includes(keyword.toLowerCase())
         }))
         .then((restaurants) => res.render('index', { restaurant: restaurants, keyword }))
-        .catch(error => console.log(error))
-})
-
-// create
-app.get('/restaurants/new', (req, res) => {
-    console.log("This is create")
-    return res.render('new')
-})
-
-app.post('/restaurants', (req, res) => {
-    const restaurant = req.body
-    return Restaurant.create(restaurant)     // 存入資料庫
-        .then(() => res.redirect('/')) // 新增完成後導回首頁
-        .catch(error => console.log(error))
-})
-
-// detail
-app.get('/restaurants/:restaurant_id', (req, res) => {
-    const id = req.params.restaurant_id
-    return Restaurant.find()
-        .lean()
-        .then((restaurants) => restaurants.filter(restaurant => restaurant._id.toString() === id))
-        .then((restaurant) => restaurant[0])
-        .then((restaurant) => res.render('show', { restaurant: restaurant }))
-        .catch(error => console.log(error))
-})
-
-// edit
-app.get('/restaurants/:restaurant_id/edit', (req, res) => {
-    const id = req.params.restaurant_id
-    return Restaurant.find()
-        .lean()
-        .then((restaurants) => restaurants.filter(restaurant => restaurant._id.toString() === id))
-        .then((restaurant) => restaurant[0])
-        .then((restaurant) => res.render('edit', { restaurant: restaurant }))
-        .catch(error => console.log(error))
-})
-
-app.post('/restaurants/:restaurant_id/edit', (req, res) => {
-    const id = req.params.restaurant_id
-    const name = req.body.name
-    const name_en = req.body.name_en
-    const category = req.body.category
-    const location = req.body.location
-    const google_map = req.body.google_map
-    const phone = req.body.phone
-    const description = req.body.description
-    const image = req.body.image
-
-    return Restaurant.findById(id)
-        .then(restaurant => {
-            restaurant.name = name
-            restaurant.name_en = name_en
-            restaurant.category = category
-            restaurant.location = location
-            restaurant.google_map = google_map
-            restaurant.phone = phone
-            restaurant.description = description
-            restaurant.image = image
-
-            return restaurant.save()
-        })
-        .then(() => res.redirect(`/restaurants/${id}`))
-        .catch(error => console.log(error))
-})
-
-// delete
-app.post('/restaurants/:id/delete', (req, res) => {
-    const id = req.params.id
-    return Restaurant.findById(id)
-        .then(restaurant => {
-            return restaurant.deleteOne()
-        })
-        .then(() => res.redirect('/'))
         .catch(error => console.log(error))
 })
 
---
+++ routes/modules/home.js
@@ -0,0 +1,18 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+// 引用 Todo model
+const Restaurant = require('../../models/Restaurant')
+// 定義首頁路由
+
+// index
+router.get('/', (req, res) => {
+    Restaurant.find()
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .sort({ name: 'asc' })
+        .then(restaurant => res.render('index', { restaurant })) // 將資料傳給 index 樣板
+        .catch(error => console.error(error)) // 錯誤處理
+})
+
+// 匯出路由模組
+module.exports = router
